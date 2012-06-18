377054012860174449545cec8267b5405bba3bbd
Go Frendi Gunawan <gofrendiasgard@gmail.com>
2012-06-18 19:18:12 +0700

    Add questions (done by Sonny)

--- controllers/typing_defense.php
+++ controllers/typing_defense.php
@@ -6,28 +6,101 @@
  */
 class Typing_defense extends CMS_Controller {
     //put your code here
-    public function index(){        
-    	if($this->cms_allow_navigate('typedef_index')){
-        	$this->load->view('index.php');
-    	}else{
-    		echo "login first";
-    		//replace this with redirect
+    public function index(){
+    	$query = $this->db
+    		->select_sum('score')
+    		->from('typedef_score')
+    		->where('user_id', $this->cms_userid())
+    		->get();
+    	$row = $query->row();
+    	$user_score = isset($row->score)?$row->score:0;
+    	
+    	$where = 'min_score_to_play <= '.$user_score;
+    	$query = $this->db
+    		->select('level_id, level_name, description')
+    		->from('typedef_level')
+    		->where($where)
+    		->order_by('min_score_to_play', 'desc')
+    		->get();
+    	$games = array();
+    	foreach($query->result() as $row){
+    		$games[] = array(
+    			'id' => $row->level_id,
+    			'name' => $row->level_name,
+    			'description' => $row->description
+    		);
     	}
+    	$data=array("games"=>$games);
+    	$this->view('index.php', $data, 'typedef_index');
+    }
+    
+    public function game($level_id=NULL){
+    	if(!isset($level_id)){    		
+    		redirect($this->cms_module_name().'/index');
+    	}
+    	$data=array(
+    			"level" => $level_id,
+    	);
+    	$this->load->view('game.php', $data);
+    }
+    
+    public function json_end_game($level_id){
+    	$query = $this->db->select('wrong_penalty, collation_penalty, correct_point, interval, speed, win_score')
+    	->from('typedef_level')
+    	->where('level_id', $level_id)
+    	->get();
+    	$row = $query->row();
+    	$win_score = $row->win_score;
+    	
+    	$user_id = $this->cms_userid();
+    	$score = $this->input->post('score');
+    	$where = array("level_id"=>$level_id, "user_id"=>$user_id);
+    	$query = $this->db->select('score_id, score, win')
+    		->from('typedef_score')
+    		->where($where)
+    		->get();
+    	
+    	if($query->num_rows()==0){
+    		$win = ($score >= $win_score)?1:0;
+    		if($win) $score = $win_score;
+    		$data = array("level_id"=>$level_id, "user_id"=>$user_id, "score"=>$score, "win"=>$win);
+    		$this->db->insert('typedef_score', $data);
+    	}else{
+    		$row = $query->row();
+    		$old_score = $row->score;
+    		if($score>$old_score){
+    			$win = ($score >= $win_score)?1:0;
+    			if($win) $score = $win_score;
+    			$data = array("level_id"=>$level_id, "user_id"=>$user_id, "score"=>$score, "win"=>$win);
+    			$where = array("score_id"=>$row->score_id);
+    			$this->db->update('typedef_score', $data, $where);
+    		}
+    	}
+    	
     }
 
-    public function json_game($level_id){
+    public function json_start_game($level_id){
     	$data = array();
-    	$this->db->select('question, answer')->from('typedef_question')->where('level_id', $level_id);
-    	$query = $this->db->get();
+    	$query = $this->db->select('question, answer')
+    		->from('typedef_question')
+    		->where('level_id', $level_id)
+    	    ->get();
     	foreach($query->result() as $row){
     		$data[] = array($row->question, $row->answer, 0);
     	}
-    	$wrong_penalty = 5;
-    	$collation_penalty = 50;
-    	$correct_point = 10;
-    	$interval = 1000;
-    	$speed = 5;
-    	$win_score = 1000;
+    	
+    	$query = $this->db->select('wrong_penalty, collation_penalty, correct_point, interval, speed, win_score')
+    		->from('typedef_level')
+    		->where('level_id', $level_id)
+    		->get();
+    	$row = $query->row();
+    	$wrong_penalty = $row->wrong_penalty;
+    	$collation_penalty = $row->collation_penalty;
+    	$correct_point = $row->correct_point;
+    	$interval = $row->interval;
+    	$speed = $row->speed;
+    	$win_score = $row->win_score;
+    	
     	$json = array(
     			"data"=>$data,
     			"wrong_penalty"=>$wrong_penalty,
